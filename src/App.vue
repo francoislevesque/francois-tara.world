9c5787e11b5c4160ac97949503a0ef1ddbb1e33d
François Lévesque <francois@witify.io>
2024-04-06 18:12:34 -0400

    Tailwind and change of home page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="py-20">
+    <div class="max-w-2xl mx-auto">
+      <div class="text-center">
+        <h1 class="font-display font-medium text-4xl">
+          François & Tara
+        </h1>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
