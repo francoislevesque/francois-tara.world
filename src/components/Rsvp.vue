e5b6727301976422c15cfa96e770e3df4eafc449
François Lévesque <francois@witify.io>
2024-08-30 19:57:36 -0400

    Fix

--- src/components/Rsvp.vue
+++ src/components/Rsvp.vue
@@ -1,7 +1,7 @@
 <template>
     <a 
             href="https://forms.gle/wsD3GasDUH87QGpKA" 
-            class="bg-white duration-200 px-20 py-4 text-slate-900 font-semibold border-2 border-white hover:bg-opacity-10 hover:text-white rounded"
+            class="bg-white duration-200 px-20 py-4 text-slate-900 font-semibold border-2 border-white hover:bg-opacity-10 hover:text-white"
             target="_blank"
             >
             RSVP 
