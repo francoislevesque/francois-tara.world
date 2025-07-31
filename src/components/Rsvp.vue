821c8605c978cee6e105b5427c1b2d1b376dae6c
François Lévesque <francois@witify.io>
2025-07-31 13:02:31 -0400

    Updates

--- src/components/Rsvp.vue
+++ src/components/Rsvp.vue
@@ -1,7 +1,6 @@
 <template>
   <a
-    disabled
-    class="bg-white/40 cursor-not-allowed duration-200 h-16 px-10 inline-flex justify-center items-center tracking-wide text-slate-900 uppercase text-lg font-medium font-display hover:bg-opacity-90"
+    class="duration-200 h-16 px-10 inline-flex justify-center items-center tracking-wide text-slate-900 uppercase text-lg font-medium font-display hover:bg-opacity-90"
     target="_blank"
   >
     {{ $t("confirm_your_presence") }}
